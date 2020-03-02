4e95020cabb50b734f3e3ad7a5b692dbdb27c5b0
stanfordgolf <seanrkelman@gmail.com>
2020-03-01 18:38:43 -0800

    0.0.1 small visual improvements to menu.html and reorganization of files

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php
 include 'menu.html'; ?>
 <body id="home">
-    <div id="div1"> 
+    <div id="div1">
         <h1>Growing Up STEM</h1>
     </div>
-            <div id="div2"> 
+            <div id="div2">
             <h2>Who Are We?</h2>
             <p>
                 Growing Up STEM is a student-run organization at Los Alamitos High School that seeks to provide STEM (Science, Technology, Engineering, and Mathematics) opportunities for the young minds of Los Al and beyond. Through these activities, we hope to inspire and instill an appreciation for the sciences among our youth, and, as they grow, to provide outlets in which they can explore their STEM-based interests.
             </p>
             <button><a href = about.php>Learn More</a></button>
             </div>
-            
-            <div id="div2"> 
+
+            <div id="div2">
             <h2>News and Updates</h2>
             <p>
                 November 7, 2019 -Lee Elementary Science Discovery Night with Growing Up STEM
                 <br>Growing Up STEM hosted its second Science Night at Lee Elementary for students grades K-5, featuring new exciting experiments, including Elephant Toothpaste and Ice Cream!
             </p>
                 <button><a href = events.php>Read More</a></button>
-                
+
                 <hr>
-                
+
                 October 18, 2019 -Growing Up STEM hosts first event of the 2019-2020 school year: Hands-On Science Night at Weaver Elementary
                 <br>Our Family Science Night was a great success! With the assistance of Los Alamitos High School science students, Weaver students from Kindergarten to 5th Grade participated in a variety of hands-on science experiments, including Oobleck, Lemon-and-Baking Soda volcanos, and Dry Ice Bubbles!
             </p>
                 <button><a href = events.php>Read More</a></button>
-            
+
             </div>
 
 </body>
