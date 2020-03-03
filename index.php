7d6cdfa66077b6b56042751a3a998e9035c85700
stanfordgolf <seanrkelman@gmail.com>
2020-03-02 17:47:41 -0800

    0.0.1 improvements to look of buttons on index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include 'menu.html'; ?>
             <p>
                 Growing Up STEM is a student-run organization at Los Alamitos High School that seeks to provide STEM (Science, Technology, Engineering, and Mathematics) opportunities for the young minds of Los Al and beyond. Through these activities, we hope to inspire and instill an appreciation for the sciences among our youth, and, as they grow, to provide outlets in which they can explore their STEM-based interests.
             </p>
-            <button><a href = about.php>Learn More</a></button>
+            <a href="about.php" class="button button1">Learn More</a>
             </div>
 
             <div id="div2">
@@ -17,16 +17,17 @@ include 'menu.html'; ?>
             <p>
                 November 7, 2019 -Lee Elementary Science Discovery Night with Growing Up STEM
                 <br>Growing Up STEM hosted its second Science Night at Lee Elementary for students grades K-5, featuring new exciting experiments, including Elephant Toothpaste and Ice Cream!
-            </p>
-                <button><a href = events.php>Read More</a></button>
+            </p
+                <a href = "about.php" class = "button button1">Read More</a>
 
                 <hr>
 
                 October 18, 2019 -Growing Up STEM hosts first event of the 2019-2020 school year: Hands-On Science Night at Weaver Elementary
                 <br>Our Family Science Night was a great success! With the assistance of Los Alamitos High School science students, Weaver students from Kindergarten to 5th Grade participated in a variety of hands-on science experiments, including Oobleck, Lemon-and-Baking Soda volcanos, and Dry Ice Bubbles!
             </p>
-                <button><a href = events.php>Read More</a></button>
+                <a href = "about.php" class = "button button1">Read More</a>
 
             </div>
 
 </body>
+</html>
